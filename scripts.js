956398fe2f8acbdb4c587009637468a09024f927
Lou Cairns <loucairns907@gmail.com>
2023-03-27 20:55:22 +0100

    update

---
+++ scripts.js
@@ -0,0 +1,31 @@
+
+//TRYING TO WORK OUT HOW TO INSERT LIVE DATE
+//const today = new Date();
+
+
+//TRYING TO WORK OUT HOW TO DO IMAGE CHANGE ON CLICK
+
+   // let user;
+
+   // function yourName() {
+    //  user = prompt("What is your name?");
+      //return document.write(user);
+    //}
+ 
+
+//ABSOLUTELY NO IDEA HERE
+
+    //function guessANumber() {let user;
+
+//function yourName() {user = prompt("TYPE YOUR NAME");
+
+let user;
+
+function yourName() {user = prompt("Who are you?");
+
+alert("Welcome " + user ); 
+
+console.log(user); return user;
+}
+
+      
